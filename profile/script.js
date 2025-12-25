656f2343a400589515a761ffa8da3fcaaf6d6b33
msson04 <sonminseok0219@naver.com>
2025-12-25 23:10:11 +0900

    Add files via upload

---
+++ profile/script.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+document.querySelectorAll(".fade-up, .fade-in").forEach(el => {
+  observer.observe(el);
+});
